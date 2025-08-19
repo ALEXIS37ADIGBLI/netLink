d0010016816dc2221f8f359d3a5b821eb38d3f2b
ALEXIS37ADIGBLI <154370828+ALEXIS37ADIGBLI@users.noreply.github.com>
2025-08-19 09:17:37 +0000

    Réalisation de la page login

--- netLink/app/Login.jsx
+++ netLink/app/Login.jsx
@@ -1,16 +1,121 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Alert, Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useRef, useState } from 'react'
 import ScreenWrapper from '../components/ScreenWrapper'
+import { theme } from '../constants/theme'
+import Icon from '../assets/icons'
+import { StatusBar } from 'expo-status-bar'
+import BackButton from '../components/BackButton'
+import { useRouter } from 'expo-router'
+import { hp, wp } from '../helpers/common'
+import Input from '../components/input'
+import Button from '../components/Button'
 
 
 const Login = () => {
+    const router = useRouter();
+    const emailRef = useRef("");
+    const passwordRef = useRef("");
+    const [loading, setLoading] = useState(false);
+
+    const onSubmit = async () =>  {
+      if(!emailRef.current || !passwordRef.current){
+        Alert.alert('Login', "Svp entrez tous les champs")
+        return;
+      }
+    }
+
   return (
-    <ScreenWrapper>
-      <Text>Login</Text>
+    <ScreenWrapper bg='white'>
+        <StatusBar style='dark'/>
+        <View style={styles.container}>
+            <BackButton router={router}/>
+            {/*Text de bienvenue */}
+            <View>
+              <Text style={styles.welcomeText}>
+                Salut,
+              </Text>
+              <Text style={styles.welcomeText}>
+                bienvenue de nouveau
+              </Text>
+            </View>
+
+            {/*Formulaire */}
+            <View style={styles.form}>
+              <Text style={{fontSize: hp(1.5), color: theme.colors.text}}>
+                Veuillez vous connecter pour continuer
+              </Text>
+              <Input 
+                icon={<Icon name='mail' size={26} strokeWidth={1.6}/>}
+                placeholder='Entrez votre email'
+                onChangeText={value=> emailRef.current = value}
+              />
+
+              <Input 
+                icon={<Icon name='lock' size={26} strokeWidth={1.6}/>}
+                placeholder='Entrez votre Mot de passe'
+                secureTextEntry
+                onChangeText={value=> passwordRef.current = value}
+              />
+
+              <Text style={styles.forgotPassword}>
+                Mot de passe oublié?
+              </Text>
+
+              {/*bouttons */}
+              <Button title={'Se connecter'} loading={loading} onPress={onSubmit}/> 
+            </View>
+
+            {/*footer */}
+            <View style={styles.footer}>
+              <Text style={styles.footerText}>
+                Pas de compte ?
+              </Text>
+              <Pressable>
+                <Text style={[styles.footerText, {color: theme.colors.primaryDark, fontWeight: theme.fonts.semibold}]}>S'inscrire</Text>
+              </Pressable>
+            </View>
+        </View>
     </ScreenWrapper>
   )
 }
 
 export default Login
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 45,
+    paddingHorizontal: wp(5)
+  },
+
+  welcomeText: {
+    fontSize: hp(4),
+    fontWeight: theme.fonts.bold,
+    color: theme.colors.text,
+  },
+
+  form: {
+    gap: 25
+  },
+
+  forgotPassword: {
+    textAlign: 'right',
+    fontWeight: theme.fonts.semibold,
+    color: theme.colors.text
+  },
+
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 5
+  },
+
+  footerText: {
+    textAlign: 'center',
+    color: theme.colors.text,
+    fontSize: hp(1.6)
+
+  }
+
+})
