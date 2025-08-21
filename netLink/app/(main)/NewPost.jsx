eb94974b64259be552fb7589fcb80e59e91a5430
ALEXIS37ADIGBLI <154370828+ALEXIS37ADIGBLI@users.noreply.github.com>
2025-08-21 20:58:38 +0000

    réalisation du design de la page de création de post

--- netLink/app/(main)/NewPost.jsx
+++ netLink/app/(main)/NewPost.jsx
@@ -1,14 +1,164 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useRef, useState } from "react";
+import ScreenWrapper from "../../components/ScreenWrapper";
+import Header from "../../components/Header";
+import { hp, wp } from "../../helpers/common";
+import { theme } from "../../constants/theme";
+import { useAuth } from "../../context/AuthContext";
+import Avatar from "../../components/Avatar";
+import RichTextEditor from "../../components/RichTextEditor";
+import { useRouter } from "expo-router";
+import Icon from "../../assets/icons";
+import Button from "../../components/Button";
 
 const NewPost = () => {
+  const { user } = useAuth();
+  const bodyRef = useRef("");
+  const editorRef = useRef(null);
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [file, setFile] = useState(file);
+
+  const onPick = async (isImage) =>{
+
+  }
+
+  const onSubmit = async ()=> {
+    
+  }
   return (
-    <View>
-      <Text>NewPost</Text>
-    </View>
-  )
-}
+    <ScreenWrapper bg="white">
+      <View style={styles.container}>
+        <Header title="Créer un post" />
+        <ScrollView contentContainerStyle={{ gap: 20 }}>
+          <View style={styles.header}>
+            <Avatar
+              uri={user?.image}
+              size={hp(6.5)}
+              rounded={theme.radius.xl}
+            />
+            <View style={{ gap: 2 }}>
+              <Text style={styles.username}>{user && user.name}</Text>
+              <Text style={styles.publicText}>Public</Text>
+            </View>
+          </View>
+          <View style={styles.textEditor}>
+            <RichTextEditor
+              editorRef={editorRef}
+              onChange={(body) => (bodyRef.current = body)}
+            />
+          </View>
+          <View style={styles.media}>
+            <Text style={styles.addImageText}>Ajoutez à votre poste</Text>
+            <View style={styles.mediaIcons}>
+                <TouchableOpacity onPress={()=> onPick(true)}>
+                    <Icon name='image' size={30} color={theme.colors.darkGray}/>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={()=> onPick(false)}>
+                    <Icon name='video' size={33} color={theme.colors.darkGray}/>
+                </TouchableOpacity>
+            </View>
+          </View>
+        </ScrollView>
+        <Button 
+            buttonStyle={{height: hp(6.2)}}
+            title="Poster"
+            loading={loading}
+            hasShadow={false}
+            onPress={onSubmit}
+        />
+      </View>
+    </ScreenWrapper>
+  );
+};
 
-export default NewPost
+export default NewPost;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginBottom: 30,
+    paddingHorizontal: wp(4),
+    gap: 15,
+  },
+
+  title: {
+    fontSize: hp(2.5),
+    fontWeight: theme.fonts.semibold,
+    color: theme.colors.text,
+    textAlign: "center",
+  },
+
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12,
+  },
+
+  username: {
+    fontSize: hp(2.2),
+    fontWeight: theme.fonts.semibold,
+    color: theme.colors.text,
+  },
+
+  avatar: {
+    height: hp(6.5),
+    width: hp(6.5),
+    borderRadius: theme.radius.xl,
+    borderCurve: "continuous",
+    borderWidth: 1,
+    borderColor: "rgba(0,0,0,0.1)",
+  },
+
+  publicText: {
+    fontSize: hp(1.7),
+    fontWeight: theme.fonts.medium,
+    color: theme.colors.textLight,
+  },
+
+  textEditor: {},
+
+  media: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderWidth: 1.5,
+    padding: 12,
+    paddingHorizontal: 18,
+    borderRadius: theme.radius.xl,
+    borderCurve: "continuous",
+    borderColor: theme.colors.darkGray,
+  },
+
+  mediaIcons: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 15,
+  },
+
+  addImageText: {
+    fontSize: hp(1.9),
+    fontWeight: theme.fonts.semibold,
+    color: theme.colors.text,
+  },
+
+  imageIcon: {
+    borderRadius: theme.radius.md,
+  },
+
+  file: {
+    height: hp(30),
+    width: "100%",
+    borderRadius: theme.radius.xl,
+    overflow: "hidden",
+    borderCurve: "continuous",
+  },
+
+  video: {},
+
+  closeIcon: {
+    position: "absolute",
+    top: 10,
+    right: 10,
+  },
+});
---
+++ netLink/components/RichTextEditor.jsx
@@ -0,0 +1,69 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import {
+  actions,
+  RichEditor,
+  RichToolbar,
+} from "react-native-pell-rich-editor";
+import { theme } from "../constants/theme";
+
+const RichTextEditor = ({ editorRef, onChange }) => {
+  return (
+    <View style={{ minHeight: 285 }}>
+  <RichToolbar
+    editor={editorRef}
+    actions={[
+      actions.setBold,
+      actions.setItalic,
+      actions.insertBulletsList,
+      actions.insertOrderedList,
+      actions.insertLink,
+    ]}
+    style={styles.richBar}
+    selectedIconTint={theme.colors.primaryDark}
+    flatContainerStyle={styles.flatStyle}
+  />
+
+  <RichEditor
+    ref={editorRef}
+    containerStyle={styles.rich}
+    editorStyle={{
+      backgroundColor: 'white',
+      color: 'black',
+      placeholderColor: 'gray',
+      cssText: 'body {font-size: 16px;}',
+    }}
+    placeholder="Votre pensée..."
+    onChange={onChange}
+    initialHeight={200}
+  />
+</View>
+
+  );
+};
+
+export default RichTextEditor;
+
+const styles = StyleSheet.create({
+  richBar: {
+    borderTopRightRadius: theme.radius.xl,
+    borderTopLeftRadius: theme.radius.xl,
+    backgroundColor: theme.colors.lightGray,
+  },
+
+  rich: {
+    minHeight: 240,
+    flex: 1,
+    borderWidth: 1.5,
+    borderTopWidth: 0,
+    borderBottomLeftRadius: theme.radius.xl,
+    borderBottomRightRadius: theme.radius.xl,
+    borderColor: theme.colors.darkGray,
+    padding: 5,
+  },
+
+  flatStyle: {
+    paddingHorizontal: 8,
+    gap: 3,
+  }
+});
